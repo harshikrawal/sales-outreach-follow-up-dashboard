4b574a36ab36b5ec11d6474d5e136511c2187c2d
Harshik Rawal <147087686+harshikrawal@users.noreply.github.com>
2026-08-17 11:29:46 +0530

    style: remove flex class from body to stop browser horizontal scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex text-foreground bg-background">
+      <body className="min-h-full text-foreground bg-background">
         <AppLayout>
           {children}
         </AppLayout>
